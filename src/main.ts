a374eab2f197cfcb24b6210b4dc37b46d515aa7e
Ella B <ella55ball@gmail.com>
2025-07-06 21:33:31 +0100

    began to structure html, set up scss pages for variables, begin initialising landing screen for game content

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.scss";
+import "./variables.scss";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const startGameButton = document.querySelector(
+    ".start-game-btn"
+) as HTMLButtonElement;
+const landingContent = document.querySelector(
+    ".landing-screen"
+) as HTMLDivElement;
+const gameContent = document.querySelector(".game-content") as HTMLDivElement;
+// function to change display of any html element
+function setElementVisibility(element, display: boolean) {
+    element.style.display = display ? "block" : "none";
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function startGame() {
+    setElementVisibility(landingContent, false);
+    setElementVisibility(gameContent, true);
+}
+
+startGameButton.addEventListener("click", startGame);
+
+//pizza icons
