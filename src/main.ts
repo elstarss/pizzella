854489466df91af51394a80883ec719ef15c2b03
Ella B <ella55ball@gmail.com>
2025-07-06 22:07:34 +0100

    added function to generate a random order based on array of toppings. created function to log ingredients clicked on. created function to check each of these arrays against each other

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,72 @@ function setElementVisibility(element, display: boolean) {
 function startGame() {
     setElementVisibility(landingContent, false);
     setElementVisibility(gameContent, true);
+    generateOrder(2);
 }
 
 startGameButton.addEventListener("click", startGame);
 
-//pizza icons
+// gameplay content
+// ..
+// generating customer order
+const toppingsList: string[] = [
+    "tomato sauce",
+    "cheese",
+    "mushroom",
+    "sliced tomato",
+];
+
+// creating a function to shuffle array using fisher yates
+// goes through array starting from last index and swaps random elements with the current [i]. copies array so it is not mutated
+function shuffle(array: string[]): string[] {
+    const arrayCopy = [...array];
+    for (let i = arrayCopy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+    }
+    return arrayCopy;
+}
+
+// customerOrder globally accessible
+let customerOrder: string[] = [];
+
+// generate order accepts numberoftoppings so i can make the game harder later on. always has a pizza base
+function generateOrder(numberOfToppings: number) {
+    const shuffledToppings = shuffle(toppingsList);
+    const slicedToppings = shuffledToppings.slice(0, numberOfToppings);
+    slicedToppings.push("base");
+    customerOrder = slicedToppings;
+    console.log("Customer order is: " + customerOrder);
+}
+
+let clickedIngredientsArray: string[] = [];
+
+// function captures which ingredients are clicked by player
+function clickedIngredients(topping: string) {
+    if (!clickedIngredientsArray.includes(topping)) {
+        clickedIngredientsArray.push(topping);
+        console.log(`Clicked ingredients array is ${clickedIngredientsArray}`);
+    }
+}
+
+// add event listener to all ingredients and apply clickedIngredients function
+
+// checking if clicked ingredient array matches generated customer order array
+let winCount: number = 0;
+//
+function checkOrder() {
+    let correctIngredients = 0;
+    for (let i = 0; i < clickedIngredientsArray.length; i++) {
+        if (customerOrder.includes(clickedIngredientsArray[i])) {
+            console.log(correctIngredients);
+            correctIngredients++;
+            console.log(correctIngredients);
+        }
+    }
+    if (correctIngredients === clickedIngredientsArray.length) {
+        console.log("correct");
+        winCount++;
+    } else {
+        console.log("wrong toppings!");
+    }
+}
